f08c200af77674f251b54af1984359a916de957e
Miles Smith <miles-smith@omrf.org>
2019-03-28 10:47:50 -0500

    Added color palette arguments to enhancedDimPlot

--- R/enhancedDimPlot.R
+++ R/enhancedDimPlot.R
@@ -17,6 +17,9 @@
 #' @param label Should labels be shown? Default: TRUE
 #' @param label_size Label font size. Default: 3
 #' @param label_text_color Label font color. Default: black
+#' @param color_palette Palette to use.  If \code{fill_palette} is provided, this controls
+#' the color surrounding objects and the background of labels. Default: palette "bold_color" from package "rcartocolor"
+#' @param fill_palette Palette to use for point fill.  By default, this matches \code{color_palette}
 #' @param ... Additional parameters
 #'
 #' @importFrom dplyr enquo quo_name inner_join filter group_by summarise
@@ -53,6 +56,8 @@ enhancedDimPlot.Seurat <- function(object,
                                    label = TRUE,
                                    label_size = 3,
                                    label_text_color = 'black',
+                                   color_palette = NULL,
+                                   fill_palette = NULL,
                                    ...){
   try(
     if (is.null(grouping_var)){
@@ -94,25 +99,25 @@ enhancedDimPlot.Seurat <- function(object,
   dim_1 <- dimNames[[1]]
   dim_2 <- dimNames[[2]]
 
-  plot.data <- dimData %>%
+  plotData <- dimData %>%
     as.data.frame() %>%
     rownames_to_column('cell') %>%
     inner_join(metaData,
                by = 'cell')
 
   if (!is.null(group_plot)){
-    plot.data %<>% filter(!!grouping_var == group_plot)
+    plotData %<>% filter(!!grouping_var == group_plot)
   }
 
-  plot.data[["x"]] <- plot.data[, dim_1]
-  plot.data[["y"]] <- plot.data[, dim_2]
+  plotData[["x"]] <- plotData[, dim_1]
+  plotData[["y"]] <- plotData[, dim_2]
 
-  centers <- plot.data %>%
+  centers <- plotData %>%
     group_by(!!grouping_var) %>%
     summarise(x = median(x = x),
               y = median(x = y))
 
-  p1 <- plot.data %>%
+  pl <- plotData %>%
     ggplot(aes(x = x,
                y = y,
                color = !!grouping_var,
@@ -122,18 +127,18 @@ enhancedDimPlot.Seurat <- function(object,
                shape = 21)
 
   if (isTRUE(faceting)){
-    p1 <- p1 + facet_wrap(quo_name(split_by))
+    pl <- pl + facet_wrap(quo_name(split_by))
   }
 
   if (label){
-    p2 <- p1 +
+    pl <- pl +
       geom_point(data = centers,
                  mapping = aes(x = x,
                                y = y),
                  size = 0,
                  alpha = 0)
     if (isTRUE(faceting)){
-      p2 <- p2 + geom_label_repel(data = centers,
+      pl <- pl + geom_label_repel(data = centers,
                                   mapping = aes( x = x,
                                                  y = y,
                                                  label = !!grouping_var,
@@ -145,7 +150,7 @@ enhancedDimPlot.Seurat <- function(object,
                                   force = force,
                                   inherit.aes = FALSE)
     } else {
-      p2 <- p2 + geom_label_repel(data = centers,
+      pl <- pl + geom_label_repel(data = centers,
                                   mapping = aes( x = x,
                                                  y = y,
                                                  label = !!grouping_var,
@@ -156,11 +161,19 @@ enhancedDimPlot.Seurat <- function(object,
                                   force = force,
                                   inherit.aes = FALSE)
     }
-
-
-  } else {
-    p2 <- p1
   }
 
-  p2 + theme(legend.position = "none")
+  if (!is.null(color_palette)){
+    color_palette <- PrepPalette(palette_use = color_palette,
+                                 bins = length(unique(plotData[[quo_name(grouping_var)]])))
+    if (is.null(fill_palette)){
+      fill_palette <- color_palette
+    } else {
+      color_palette <- PrepPalette(palette_use = fill_palette,
+                                   bins = length(unique(plotData[[quo_name(grouping_var)]])))
+    }
+    pl <- pl + scale_color_manual(values = color_palette) + scale_fill_manual(values = color_palette)
+  }
+
+  pl + theme(legend.position = "none")
 }
